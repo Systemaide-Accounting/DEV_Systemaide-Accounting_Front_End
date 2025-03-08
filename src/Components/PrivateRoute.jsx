ec34278e2999218ae9ee7f25bd11c0f032afacbe
MrDrich <inigoaldrich24@gmail.com>
2025-03-08 20:15:21 +0800

    Done successful login alert

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,16 +1,27 @@
+import swal2 from "sweetalert2";
 import { Outlet } from "react-router-dom";
 import { Dashboard } from "./Dashboard";
 import { useEffect } from "react";
 import { checkUserExpiration } from "../hooks/checkUserExpiration";
 
 export function PrivateRoute() {
-  
+
   useEffect(() => {
-      const checkAuthentication = async () => {
-        await checkUserExpiration();
-      };
-      checkAuthentication();
-    }, []);
+    const checkAuthentication = async () => {
+      await checkUserExpiration();
+    };
+    checkAuthentication();
+    
+    const successfulLoginAlertShown = localStorage.getItem("successfulLoginAlertShown");
+    if (!successfulLoginAlertShown) {
+      swal2.fire({
+        title: "Successfully logged in",
+        text: "Redirecting to home page",
+        icon: "success",
+      });
+      localStorage.setItem("successfulLoginAlertShown", true);
+    }
+  }, []);
   
   return (
     <>
